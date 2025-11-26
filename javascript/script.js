adac44550ff88cb838b1e9b9e597f0870bcd3126
cesarfcribeiro <cesarfcribeiro@gmail.com>
2025-11-26 17:51:28 -0300

    commit final

--- javascript/script.js
+++ javascript/script.js
@@ -5,4 +5,57 @@ $(function () {
     // Alterna entre o ícone de "menu" (fa-bars) e o "X" (fa-xmark)
     $(this).find("i").toggleClass("fa-bars fa-xmark");
   });
+
+  const sections = $("section");
+  const navItems = $(".nav-item");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    let activeSectionIndex = 0;
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.2)");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false; // Sai do loop each
+      }
+    });
+    navItems.removeClass("active");
+    $(navItems[activeSectionIndex]).addClass("active");
+  });
+
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
+
+  ScrollReveal().reveal(".dish", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
+
+  ScrollReveal().reveal("#testimonial_chef", {
+    origin: "left",
+    duration: 3000,
+    distance: "20%",
+  });
+
+  ScrollReveal().reveal(".feedback", {
+    origin: "right",
+    duration: 2000,
+    distance: "20%",
+  });
 });
